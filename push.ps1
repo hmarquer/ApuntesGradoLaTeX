c679c33adae82cb8c8d374fdac65b2ccb3345816
Hugo Marquerie Martín <hugomarquerie@gmail.com>
2024-06-19 16:07:45 +0200

    miércoles 19/06/2024 16:07

--- push.ps1
+++ push.ps1
@@ -2,4 +2,6 @@ $DST = Get-Date -UFormat  "%A %d/%m/%Y %R"
 git add *
 git commit -m $DST
 git push
+Copy-Item ".\Probabilidad I\build\prob.pdf" -Destination "..\2 SEGUNDO CURSO\2.2 PROBABILIDAD I"
 Copy-Item ".\Ecuaciones diferenciales\build\edo.pdf" -Destination "..\2 SEGUNDO CURSO\2.2 ECUACIONES DIFFERENCIALES"
+Copy-Item ".\Geometría de curvas y superficies\build\gcs.pdf" -Destination "..\2 SEGUNDO CURSO\2.2 GEOMETRÍA DE CURVAS Y SUPERFICIES"
