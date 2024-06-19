7fac7bd375c6d298496d0bf926d1e5a4813dfb31
Hugo Marquerie Martín <hugomarquerie@gmail.com>
2024-06-19 16:01:45 +0200

    miércoles 19/06/2024 16:01

--- push.ps1
+++ push.ps1
@@ -2,4 +2,4 @@ $DST = Get-Date -UFormat  "%A %d/%m/%Y %R"
 git add *
 git commit -m $DST
 git push
-Copy-Item "C:\Users\Hugo\OneDrive - UAM\ApuntesGradoLaTeX\Ecuaciones diferenciales\build\edo.pdf" -Destination "C:\Users\Hugo\OneDrive - UAM"
+Copy-Item ".\Ecuaciones diferenciales\build\edo.pdf" -Destination ".."
